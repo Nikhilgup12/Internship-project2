3227acf3456d250fffc58bbfcc3b2c846b5bc917
nikhil <nikhil@gmail.com>
2024-07-02 16:31:19 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const index = () => {
     {/* <Navbar />  */}
     <div className={styles["landing-main-container"]}>
       <div className={styles['landing-first-image-container']}>
-        <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056168/Rectangle_lqkm6z.svg" className={styles['landing-image']}/> 
+        <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056168/Rectangle_lqkm6z.svg" className={`${styles['landing-image']} ${styles['fade-in']}`}/> 
       </div>
 
       <div className={styles['landing-product-list-main-container']}>
@@ -17,28 +17,28 @@ const index = () => {
           <h1 className={styles['landing-product-list-heading']}> New Arrivals </h1>
           <hr className={styles['horizontal-line']}/> 
           <ul className={styles['product-list-container']}>
-            <li className={styles['product-list']}>
-              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={styles['product-image']}/> 
+            <li className={`${styles['product-list']}`}>
+              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={`${styles['product-image']} ${styles['product-hover']}`}/> 
               <h1 className={styles['product-heading']}> Dress </h1> 
               <p className={styles['product-price']}> Rs. 35,000.00 </p>
             </li>
             <li className={styles['product-list']}>
-              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={styles['product-image']}/> 
+              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={`${styles['product-image']} ${styles['product-hover']}`}/> 
               <h1 className={styles['product-heading']}> Dress </h1> 
               <p className={styles['product-price']}> Rs. 35,000.00 </p>
             </li>
             <li className={styles['product-list']}>
-              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={styles['product-image']}/> 
+              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={`${styles['product-image']} ${styles['product-hover']}`}/> 
               <h1 className={styles['product-heading']}> Dress </h1> 
               <p className={styles['product-price']}> Rs. 35,000.00 </p>
             </li>
             <li className={styles['product-list']}>
-              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={styles['product-image']}/> 
+              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={`${styles['product-image']} ${styles['product-hover']}`}/> 
               <h1 className={styles['product-heading']}> Dress </h1> 
               <p className={styles['product-price']}> Rs. 35,000.00 </p>
             </li>
             <li className={styles['product-list']}>
-              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={styles['product-image']}/> 
+              <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056164/Rectangle_1_sijyin.svg" className={`${styles['product-image']} ${styles['product-hover']}`}/> 
               <h1 className={styles['product-heading']}> Dress </h1> 
               <p className={styles['product-price']}> Rs. 35,000.00 </p>
             </li>
@@ -57,7 +57,7 @@ const index = () => {
         <div className={styles['landing-collection-image-container']}>
           <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719056153/Rectangle_2_xmpa30.svg" className={styles['collection-image']} /> 
         </div>
-        <div className={styles['landing-collection-detail-container']}>
+        <div className={`${styles['landing-collection-detail-container']} ${styles['slide-in']}`}>
           <h1 className={styles['collection-heading']}> NEW IN COLLECTION</h1>
           <p className={styles['collection-para']}> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh  </p> 
           <p> Explore </p>
@@ -65,48 +65,6 @@ const index = () => {
         </div>
       </div>
       <Fotter />
-      {/* <div className={styles['follow-main-container']}>
-        <div className={styles['follow-icon-container']}>
-          <FaInstagram />
-          <h1> FOLLOW US ON INSTAGRAM </h1>
-          <p> @floe.life </p>
-        </div>
-        <div className={styles['follow-image-container']}>
-          <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719123016/Rectangle_3_slyyx6.svg" className={styles['follow-image']}/>
-          <img src="https://res.cloudinary.com/di9qg5ka6/image/upload/v1719123034/Rectangle_4_rlmcmj.svg" className={styles["follows-image"]}/> 
-        </div>
-        <div className={styles['landing-fotter-container']}>
-          <div className={styles['landing-fotter-shop-container']}>
-            <h1 className={styles['fotter-shop-heading']}> SHOP </h1>
-            <ul>
-              <li> Shop all </li>
-              <li> New Arrivals </li>
-              <li> Gift Card </li>
-              <li> Wishlist </li>
-            </ul>
-          </div>
-          <div className={styles['landing-fotter-shop-container']}>
-            <h1 className={styles['fotter-shop-heading']}> CUSTOMER CARE </h1>
-            <ul>
-              <li> Shipping </li>
-              <li> Return Policy</li>
-              <li> Exchange </li>
-              <li> Size Guide </li>
-              <li> Terms and Conditions </li>
-            </ul>
-          </div>
-          <div className={styles['landing-offer-main-container']}>
-            <div className={styles['landing-offer-container']}>
-              <h1> ENJOY 10% OFFYOUR FIRST ORDER </h1>
-              <p> Sign up to our newsletter and receive exclusive access to new collections, promotions and events. </p>
-              <p> Name </p>
-              <p> Email </p>
-              <button className={styles['offer-continue-button']}> CONTINUE </button>
-            </div>
-          </div>
-        </div>
-
-      </div> */}
     </div>
     </>
   )
